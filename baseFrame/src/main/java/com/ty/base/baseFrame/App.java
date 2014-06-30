a24e3e6e1711040f0e4411157faa2168ff445371
wanghantong <hantong4510@163.com>
2014-06-30 16:39:51 +0800

    test project with no jars

---
+++ baseFrame/src/main/java/com/ty/base/baseFrame/App.java
@@ -0,0 +1,13 @@
+package com.ty.base.baseFrame;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
