a9e99e4db5e556571ee30584d6c4a6d0c0f34cc5
wanghantong <hantong4510@163.com>
2014-07-01 12:02:34 +0800

    testng template

---
+++ baseFrame/src/main/java/com/ty/base/baseFrame/BaseTestNG.java
@@ -0,0 +1,41 @@
+package com.ty.base.baseFrame;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class BaseTestNG {
+
+	@Test
+	public void testMethod() {
+	}
+
+	@BeforeMethod
+	public void beforeMethod() {
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+	}
+
+	@BeforeClass
+	public void beforeClass() {
+	}
+
+	@AfterClass
+	public void afterClass() {
+	}
+
+	@BeforeTest
+	public void beforeTest() {
+	}
+
+	@AfterTest
+	public void afterTest() {
+
+	}
+}
