3ccbb1420cd489f96678eee662d2cd3be6c65378
wanghantong <hantong4510@163.com>
2014-07-01 22:35:57 +0800

    add BaseSeleniumTemplate by testng

---
+++ baseFrame/src/main/java/com/ty/base/baseFrame/BaseSeleniumTemplate.java
@@ -0,0 +1,48 @@
+package com.ty.base.baseFrame;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class BaseSeleniumTemplate {
+
+	private WebDriver driver;
+
+	@Test
+	public void testMethod() {
+		driver.get("http://blog.csdn.net/wanghantong/article/details/28897695");
+	}
+
+	@BeforeMethod
+	public void beforeMethod() {
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+	}
+
+	@BeforeClass
+	public void beforeClass() {
+	}
+
+	@AfterClass
+	public void afterClass() {
+	}
+
+	@BeforeTest
+	public void beforeTest() {
+		driver = new FirefoxDriver();
+	}
+
+	@AfterTest
+	public void afterTest() {
+		driver.quit();
+
+	}
+}
