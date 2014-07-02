718032471a9be94c0be0f751e2179d1faf54c94c
wanghantong <hantong4510@163.com>
2014-07-02 11:28:06 +0800

    mockito base test

---
+++ baseFrame/src/main/java/com/ty/base/mockito/TestMock_Mockito.java
@@ -0,0 +1,14 @@
+package com.ty.base.mockito;
+
+import org.junit.Test;
+
+public class TestMock_Mockito {
+
+	@Test
+	public void testMockWithMockito() {
+
+		// PageObject mock = Mockito.mock(PageObject.class);
+		// Mockito.when(mock.calDate(1)).thenReturn("");
+
+	}
+}
